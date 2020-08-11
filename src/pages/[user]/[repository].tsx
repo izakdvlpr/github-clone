952fa3fa0549a46c60ff6b0638a7b9a468512631
zevdvlpr <zevdvlpr@gmail.com>
2020-08-11 13:32:54 -0300

    create repository page

--- src/pages/[user]/[repository].tsx
+++ src/pages/[user]/[repository].tsx
@@ -1,10 +1,57 @@
+import Link from 'next/link';
 import React from 'react';
 
 import Layout from '@layout';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from '@styles/pages/repository';
 
 const RepositoryPage: React.FC = () => (
   <Layout>
-    <h1>Repository</h1>
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+
+        <Link href="/zevdvlpr">
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a className="username">zevdvlpr</a>
+        </Link>
+
+        <span>/</span>
+
+        <Link href="/zevdvlpr/twitter-clone">
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a className="reponame">twitter-clone</a>
+        </Link>
+      </Breadcrumb>
+
+      <p>Clone da interface do Twitter para fins de estudo.</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href="https://github.com/zevdvlpr/twitter-clone">
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
+    </Container>
   </Layout>
 );
 
